f0f9dcc33f51939f12bd4e1be14ac241716dfffe
Andrew Seeley <awseeley@gmail.com>
2015-03-28 13:00:44 +1100

    Initial Commit

---
+++ VibrateTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VibrateTutorial
+//
+//  Created by Andrew Seeley on 28/03/2015.
+//  Copyright (c) 2015 Seemu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
